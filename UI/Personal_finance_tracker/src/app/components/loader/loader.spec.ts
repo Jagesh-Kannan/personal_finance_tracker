5927fe8e52ad820da1f58a45f3d07f8e979b7837
Jagesh-Kannan <jageshkannan15072@gmail.com>
2026-04-22 01:37:26 +0530

    Delete UI/Personal_finance_tracker directory

--- UI/Personal_finance_tracker/src/app/components/loader/loader.spec.ts
+++
@@ -1,8 +0,0 @@
-import { Loader } from './loader';
-
-describe('Loader', () => {
-  it('should create an instance', () => {
-    const directive = new Loader();
-    expect(directive).toBeTruthy();
-  });
-});
--- UI/Personal_finance_tracker/src/app/components/skeleton-loader/skeleton-loader.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SkeletonLoader } from './skeleton-loader';
-
-describe('SkeletonLoader', () => {
-  it('should create an instance', () => {
-    const directive = new SkeletonLoader();
-    expect(directive).toBeTruthy();
-  });
-});
