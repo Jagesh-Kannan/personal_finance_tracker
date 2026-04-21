804b193c6c0233945487284e4b93f6db29f9d6b3
Jagesh-Kannan <jageshkannan15072@gmail.com>
2026-04-22 01:47:17 +0530

    Delete UI/personal_finance_tracker directory

--- UI/personal_finance_tracker/src/app/interseptor/http-interseptor.service.ts
+++
@@ -1,8 +0,0 @@
-import { HttpInterceptorFn } from '@angular/common/http';
-
-export const credentialsInterceptor: HttpInterceptorFn = (req, next) => {
-  const authorizedReq = req.clone({
-    withCredentials: true // Automatically attaches cookies to every request
-  });
-  return next(authorizedReq);
-};
