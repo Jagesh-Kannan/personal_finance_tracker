4318a397bcddc5fb777b3f555b1c390716d67217
Jagesh-Kannan <jageshkannan15072@gmail.com>
2026-04-21 22:29:46 +0530

    skeliton loader, home comp changes

---
+++ UI/personal_finance_tracker/src/app/pages/home/home.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Header } from '../../components/header/header';
+import { RouterOutlet } from "@angular/router";
+
+@Component({
+  selector: 'app-home',
+  imports: [Header, RouterOutlet],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {}
