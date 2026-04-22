df4791ff2a5c7d7aff1e22ea6076a388dd4f6780
Jagesh-Kannan <jageshkannan15072@gmail.com>
2026-04-22 19:07:43 +0530

    render mode cahnge

--- UI/personal_finance_tracker/src/app/app.routes.server.ts
+++ UI/personal_finance_tracker/src/app/app.routes.server.ts
@@ -2,14 +2,24 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 import { authGuard } from './service/auth-gaurd-service';
 
 export const serverRoutes: ServerRoute[] = [
-   {
+  {
+    path: 'login',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'landing',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'home',
+    renderMode: RenderMode.Client
+  },
+  {
     path: 'reset-password/:resetToken',
     renderMode: RenderMode.Client
-
   },
   {
     path: '**',
-    renderMode: RenderMode.Prerender
-  },
-    
+    renderMode: RenderMode.Client
+  }
 ];
